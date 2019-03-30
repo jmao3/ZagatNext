80a45ffa7a0fa7df1237220eb405ccca56b73ce5
jmao3 <jmao3@artic.edu>
2019-03-30 12:52:41 -0700

    Success Data Pulling

--- pages/recipes.js
+++ pages/recipes.js
@@ -3,41 +3,39 @@ import gql from 'graphql-tag'
 import Layout from '../components/MyLayout'
 import withData from '../lib/withData'
 
-import {Link} from '../routes'
+import { Link } from '../routes'
 
 
-const AllRecipes = ({data: { loading, error, recipes }}) => {
+const AllRecipes = ({ data: { loading, error, tests } }) => {
     if (error) return <h1>Error loading reviews.</h1>
     return (
         <Layout>
-        {
-            loading ? <div>Loading</div> :
-            (
-                <div>{recipes.map((recipe, index) => 
-                    <div key={index}>
-                        <h1>
-                            <Link route='details' params={{id: recipe.id}}>
-                                <a>{recipe.title}</a>
-                            </Link>
-                        </h1>
-                        <h2>{recipe.description}</h2>
-                    </div>
-                )}</div>
-            )
-        }
+            {
+                loading ? <div>Loading</div> :
+                    (
+                        <div>{tests.map((test, index) =>
+                            <div key={index}>
+                                <h1>
+                                    <Link route='details' params={{ id: test.id }}>
+                                        <a>{test.title}</a>
+                                    </Link>
+                                </h1>
+                            </div>
+                        )}</div>
+                    )
+            }
         </Layout>
     )
     return null;
 }
 
 const allRecipes = gql`
-    query allRecipes {
-        recipes(orderBy: createdAt_DESC) {
-            id
-            createdAt
-            title
-            description
+    query AllTests {
+        tests {
+        id
+        title
         }
-    }`
+    }
+  `
 
 export default withData(graphql(allRecipes)(AllRecipes))
